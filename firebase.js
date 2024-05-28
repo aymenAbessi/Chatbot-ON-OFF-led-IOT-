6a28cf30c6839253f2b832ad82185f35c9db0385
aymenAbessi <99690345+aymenAbessi@users.noreply.github.com>
2024-05-28 17:33:40 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,12 +9,7 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCjVBKkmTLPo8QIa4yMSKv3SFPGWElf-wA",
-  authDomain: "projet-iot-effaf.firebaseapp.com",
-  projectId: "projet-iot-effaf",
-  storageBucket: "projet-iot-effaf.appspot.com",
-  messagingSenderId: "423960730386",
-  appId: "1:423960730386:web:1da30c78d7b9ecac4ab1cb"
+
 };
 
 // Initialize Firebase
